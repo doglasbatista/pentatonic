3bf32312052d29abb9a6b39c1672e68d2213ae59
doglasbatista <doglasbatista@gmail.com>
2014-09-17 15:37:32 -0300

    pag

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,33 +22,96 @@ class WelcomeController < ApplicationController
   
   
 
-  def checkout
-    unless current_user
-      flash[:alert] = "Para finalizar sua compra você 
-      precisa estar logado no sistema."
-      redirect_to new_user_session_path
+  # def checkout
+  #   unless current_user
+  #     flash[:alert] = "Para finalizar sua compra você 
+  #     precisa estar logado no sistema."
+  #     redirect_to new_user_session_path
+  #   else
+  #     if current_cart.line_items.count == 0
+  #       flash[:alert] = "Seu carrinho de compras está vazio"
+  #       redirect_to :back
+  #     else
+  #       order             = Order.new
+  #       order.user        = current_user
+  #       order.total_price = current_cart.total_price
+  #       cart              = Cart.find(params[:id])
+  #       #order           = Order.first
+  #       #order           = Cart.find(params[:id])
+  #       #payment         = PagSeguro::PaymentRequest.new
+
+  #       order.add_line_order_from_cart(cart)
+  #       order.save
+  #       logger.info "Mahh oiii"
+  #       payment = PagSeguro::PaymentRequest.new(email: 'doglasbatista@hotmail.com', token: '674953965F0B439DA05B70EF971F9E10')
+          
+
+  #       payment.reference         = order.id
+  #       payment.notification_url  = "http://104.131.58.109:3000/notification"
+  #       payment.redirect_url      = "http://104.131.58.109:3000/redirect/#{order.id}"
+
+  #       order.line_order.each do |product|
+  #         payment.items << PagSeguro::Item.new({
+  #           id: product.product.id,
+  #           description: product.product.title,
+  #           amount: product.product.price.to_f,
+  #           weight: 0
+  #           })
+  #       end
+
+  #       #payment.items = items
+  #       response      = payment.register
+
+  #       if response.errors.any?
+  #         raise response.errors.join("\n")
+  #       else
+  #         redirect_to response.url
+  #       end
+  #     end
+  #   end
+  # end
+
+
+  def save
+   @order = Order.new
+   @order.user_id = current_user.id
+   @order.total_price = current_cart.total_price
+   @order.add_line_order_from_cart(current_cart)
+   if @order.save
+     current_cart.line_items.each do |i| 
+      i.destroy
+     end
+     flash[:notice] = "Obrigado por sua compra."     
+   else
+    render :action => "checkout"
+  end
+end
+
+def checkout
+  unless current_user
+    flash[:notice] = "Para finalizar sua compra você 
+    precisa estar logado no sistema."
+    redirect_to new_user_session_path
+  else
+    if current_cart.line_items.count == 0
+      flash[:notice] = "Seu carrinho de compras está vazio"
+      redirect_to :back
     else
-      if current_cart.line_items.count == 0
-        flash[:alert] = "Seu carrinho de compras está vazio"
-        redirect_to :back
-      else
-        order           = Order.new
-        order.user      = current_user
-        order.total_price = current_cart.total_price
-        cart            = Cart.find(params[:id])
+      order = Order.new
+      cart              = Cart.find(params[:id])
         #order           = Order.first
         #order           = Cart.find(params[:id])
         #payment         = PagSeguro::PaymentRequest.new
 
         order.add_line_order_from_cart(cart)
-        order.save
+        # order.save
         logger.info "Mahh oiii"
         payment = PagSeguro::PaymentRequest.new(email: 'doglasbatista@hotmail.com', token: '674953965F0B439DA05B70EF971F9E10')
           
 
         payment.reference         = order.id
         payment.notification_url  = "http://104.131.58.109:3000/notification"
-        payment.redirect_url      = "http://104.131.58.109:3000/redirect/#{order.id}"
+        payment.redirect_url      = "http://104.131.58.109:3000/redirect/#{order}"
 
         order.line_order.each do |product|
           payment.items << PagSeguro::Item.new({
@@ -67,9 +130,9 @@ class WelcomeController < ApplicationController
         else
           redirect_to response.url
         end
-      end
     end
   end
+end
 
 
   def notification
@@ -99,7 +162,7 @@ class WelcomeController < ApplicationController
     current_cart.line_items.each do |i| 
       i.destroy
     end
-    redirect_to welcome_index_path#{}"/welcome/down_prod/order/#{@order.id}"
+    #redirect_to welcome_index_path#{}"/welcome/down_prod/order/#{@order.id}"
   end
 
   def down_prod
